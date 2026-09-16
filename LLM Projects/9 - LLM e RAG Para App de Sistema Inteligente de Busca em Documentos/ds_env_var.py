30601c29ae3e43541f13b9a3485fac9e46ff9ea8
Daniel Rezende <43621929+danielrrezende@users.noreply.github.com>
2026-09-16 17:39:30 -0300

    Add files via upload

---
+++ LLM Projects/9 - LLM e RAG Para App de Sistema Inteligente de Busca em Documentos/ds_env_var.py
@@ -0,0 +1,9 @@
+# Projeto 9 - IA Generativa e RAG Para App de Sistema Inteligente de Busca em Documentos - Backend e API
+# Módulo da API Para o LLM
+
+# Import
+import os
+
+# Nvidia API
+nvidia_key = "vazio"
+
